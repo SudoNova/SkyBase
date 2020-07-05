04f8b43386fb273ef4b44797ed1775237e05f4e2
SuNova <sunova@tutanota.com>
2020-07-05 21:09:58 +0430

    100% cov

--- src/main/java/org/skyfw/base/exception/general/TEmptyStringArgException.java
+++ src/main/java/org/skyfw/base/exception/general/TEmptyStringArgException.java
@@ -4,12 +4,24 @@ import org.skyfw.base.classes.validation.TArgValidationMCode;
 import org.skyfw.base.mcodes.TMCode;
 import org.skyfw.base.mcodes.TMCodeSeverity;
 
+/**
+ * <p>
+ * This exception is thrown whenever an empty string is parsed as method
+ * parameter.
+ * </p>
+ */
 public class TEmptyStringArgException extends TIllegalArgumentException {
 
+    /**
+     * {@inheritDoc}
+     */
     public TEmptyStringArgException(TMCode mCode, TMCodeSeverity severity, Throwable cause, String argName) {
         super(mCode, severity, cause, argName);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public TEmptyStringArgException(TMCodeSeverity severity, Throwable cause, String argName) {
         super(TArgValidationMCode.EMPTY_STRING_ARG_IS_NOT_ACCEPTABLE, severity, cause, argName);
     }
