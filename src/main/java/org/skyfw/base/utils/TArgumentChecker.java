07ec8feec467378a1003d9e482a8e53757290694
SuNova <sunova@tutanota.com>
2020-07-07 14:20:13 +0430

    Some docs

--- src/main/java/org/skyfw/base/utils/TArgumentChecker.java
+++ src/main/java/org/skyfw/base/utils/TArgumentChecker.java
@@ -2,9 +2,21 @@ package org.skyfw.base.utils;
 
 import org.skyfw.base.exception.general.TIllegalArgumentException;
 import org.skyfw.base.exception.general.TNullArgException;
-
+// TODO actually, I think this could be better as an annotation.
+/**
+ * 
+ */
 public class TArgumentChecker {
-
+    /**
+     * 
+     * @param objects Some method arguments.
+     * @throws TIllegalArgumentException Thrown if the argument is illegal.
+     * <ul>
+     * Cases of illegal argument:
+     * <li> A null argument</li>
+     * </ul>
+     * 
+     */
     public static void check(Object ...objects) throws TIllegalArgumentException {
 
         for (Object object: objects){
