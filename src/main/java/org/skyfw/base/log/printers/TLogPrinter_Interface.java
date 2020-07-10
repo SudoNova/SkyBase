1d2eaf4202332006851ddbbc764d16cd8c743433
SuNova <sunova@tutanota.com>
2020-07-10 17:31:53 +0430

    Some suggestions

--- src/main/java/org/skyfw/base/log/printers/TLogPrinter_Interface.java
+++ src/main/java/org/skyfw/base/log/printers/TLogPrinter_Interface.java
@@ -2,6 +2,7 @@ package org.skyfw.base.log.printers;
 
 import org.skyfw.base.log.TLogRecord;
 
+//TODO maybe TLogPrinter is better than TLogPrinter_Interface #suggestion
 public interface TLogPrinter_Interface {
 
     public void print(TLogRecord logRecord);
