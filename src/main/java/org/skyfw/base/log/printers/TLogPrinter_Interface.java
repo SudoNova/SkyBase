57859c075f973b0c7433b2fafb48218b627acf96
SuNova <sunova@tutanota.com>
2020-07-20 15:55:10 +0430

    Doc for print(TLogRecord)

--- src/main/java/org/skyfw/base/log/printers/TLogPrinter_Interface.java
+++ src/main/java/org/skyfw/base/log/printers/TLogPrinter_Interface.java
@@ -5,6 +5,12 @@ import org.skyfw.base.log.TLogRecord;
 //TODO maybe TLogPrinter is better than TLogPrinter_Interface #suggestion
 public interface TLogPrinter_Interface {
 
+     /**
+     * <p>
+     * Prints a {@link TLogRecord} by logger.
+     * <p>
+     * @param logRecord record to be printed.
+     */
     public void print(TLogRecord logRecord);
 
 
