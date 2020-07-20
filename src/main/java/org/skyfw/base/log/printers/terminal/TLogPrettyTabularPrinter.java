ff71d0c87d9d786bc8939c34d4aa14977bac9498
SuNova <sunova@tutanota.com>
2020-07-20 16:12:53 +0430

    1st description of TLogPrettyTabularPrinter

--- src/main/java/org/skyfw/base/log/printers/terminal/TLogPrettyTabularPrinter.java
+++ src/main/java/org/skyfw/base/log/printers/terminal/TLogPrettyTabularPrinter.java
@@ -3,55 +3,66 @@ package org.skyfw.base.log.printers.terminal;
 // TODO add to build.gradle
 import com.jakewharton.fliptables.FlipTable;
 import org.skyfw.base.log.printers.TLogPrinter_Interface;
+import org.skyfw.base.mcodes.TMCodeSeverity;
 import org.skyfw.base.log.TLogRecord;
 import org.skyfw.base.system.terminal.TTerminal;
 import org.skyfw.base.system.terminal.TTerminalTextColor;
 
 import java.text.SimpleDateFormat;
 
+/**
+ * <p>
+ * an implementation of {@link TLogPrinter_Interface} that colorizes the output,
+ * and prints it in a tabular like style, with the following columns:
+ * <ul>
+ * <li>Log Time</li>
+ * <li>Type which is an instance of {@link TMCodeSeverity}</li>
+ * <li>Class Name of the log record</li>
+ * <li>Method Name of the log record</li>
+ * <li>Method Name of the log record</li>
+ * <li>File name //TODO update
+ * <li>A {@code boolean} which shows if this record is an exception.</li>
+ * </ul>
+ * </p>
+ */
 public class TLogPrettyTabularPrinter implements TLogPrinter_Interface {
 
     @Override
     public void print(TLogRecord logRecord) {
 
-        //Var
+        // Var
         String mainTable;
         String detailsTable;
 
-        String[] headers = { "log Time", "Type", "Class Name", "Method Name",  "file Name", " exception"};
-        String[][] data = {
-                { new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(logRecord.getTimeStamp())
-                        , logRecord.getType().toString()
-                        , logRecord.getClassName()
-                        , logRecord.getMethodName()
-                        , logRecord.getFileName()
-                        , new Boolean(logRecord.isException()).toString()
-                }
-                //, {"TEEEEEEEEEEEEEEEST"}
+        String[] headers = { "log Time", "Type", "Class Name", "Method Name", "file Name", " exception" };
+        String[][] data = { { new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(logRecord.getTimeStamp()),
+                logRecord.getType().toString(), logRecord.getClassName(), logRecord.getMethodName(),
+                logRecord.getFileName(), new Boolean(logRecord.isException()).toString() }
+                // , {"TEEEEEEEEEEEEEEEST"}
         };
 
-        detailsTable= FlipTable.of(headers, data);
-        //system.out.println(mainTable.trim());
-        //system.out.println("---");
+        detailsTable = FlipTable.of(headers, data);
+        // system.out.println(mainTable.trim());
+        // system.out.println("---");
 
+        headers = new String[] { detailsTable };
 
-        headers = new String[]{detailsTable};
+        data = new String[][] { { logRecord.getDescription() } };
 
-        data = new String[][]{{logRecord.getDescription()}};
+        mainTable = FlipTable.of(headers, data);
 
-
-        mainTable= FlipTable.of(headers, data);
-
-
-
-        //mainTable= mainTable.replace("<DetailTableColor>",TTerminalTextColor.RED.toString());
-        //mainTable= mainTable.replace("</DetailTableColor>",TTerminalTextColor.CYAN.toString());
-        //mainTable= mainTable.replace("╔══",TTerminalTextColor.RED.toString() + "╔══");
-        //mainTable= mainTable.replace("══╝","══╝" + detailsTable+TTerminalTextColor.CYAN.toString());
+        // mainTable=
+        // mainTable.replace("<DetailTableColor>",TTerminalTextColor.RED.toString());
+        // mainTable=
+        // mainTable.replace("</DetailTableColor>",TTerminalTextColor.CYAN.toString());
+        // mainTable= mainTable.replace("╔══",TTerminalTextColor.RED.toString() +
+        // "╔══");
+        // mainTable= mainTable.replace("══╝","══╝" +
+        // detailsTable+TTerminalTextColor.CYAN.toString());
 
         TTerminal.reset();
 
-        switch (logRecord.getType()){
+        switch (logRecord.getType()) {
             case DEBUG:
                 TTerminal.setTextColor(TTerminalTextColor.WHITE);
                 break;
@@ -74,7 +85,6 @@ public class TLogPrettyTabularPrinter implements TLogPrinter_Interface {
 
         System.out.println(mainTable.trim());
         TTerminal.reset();
-
 
     }
 }
